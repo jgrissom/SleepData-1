fdf8f49d3497e1fb465cc2691bb13feea6302747
jgrissom <jgrissom91@gmail.com>
2023-09-05 14:12:00 -0500

    Input weeks of data needed

--- Program.cs
+++ Program.cs
@@ -1,2 +1,22 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// ask for input
+Console.WriteLine("Enter 1 to create data file.");
+Console.WriteLine("Enter 2 to parse data.");
+Console.WriteLine("Enter anything else to quit.");
+// input response
+string resp = Console.ReadLine();
+
+if (resp == "1")
+{
+    // create data file
+
+    // ask a question
+    Console.WriteLine("How many weeks of data is needed?");
+    // input the response (convert to int)
+    int weeks = int.Parse(Console.ReadLine());
+}
+else if (resp == "2")
+{
+    // TODO: parse data file
+
+}
